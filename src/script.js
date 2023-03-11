8d304bf1ec2ad605c34fabb261b2814930c6e324
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2023-03-11 16:07:57 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 const setInnerHTMLValue = (selector, value) => {
-  const element = document.querySelector(selector);
-  element.innerHTML = value;
+    const element = document.querySelector(selector);
+    element.innerHTML = value;
 };
 
 const ratioSelecter = document.getElementById("ratio");
@@ -9,60 +9,60 @@ const playerSelector = document.getElementById("player");
 const boxSelector = document.getElementById("box");
 
 const createBoxes = (ratio) => {
-  const nodes = [];
-  let count = 1;
-  for (let index = 0; index < ratio; index++) {
-    const rowDiv = document.createElement("div");
-    rowDiv.setAttribute("class", "row");
-    rowDiv.setAttribute("data-row", index);
+    const nodes = [];
+    let count = 1;
+    for (let index = 0; index < ratio; index++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.setAttribute("class", "row");
+        rowDiv.setAttribute("data-row", index);
 
-    for (let secIndex = 0; secIndex < ratio; secIndex++) {
-      const columnBox = document.createElement("div");
-      columnBox.setAttribute("class", "column");
-      rowDiv.setAttribute("data-column", secIndex);
-      columnBox.innerHTML = count;
-      count++;
-      rowDiv.appendChild(columnBox);
+        for (let secIndex = 0; secIndex < ratio; secIndex++) {
+            const columnDiv = document.createElement("div");
+            columnDiv.setAttribute("class", "column");
+            columnDiv.setAttribute("data-column", secIndex);
+            columnDiv.innerHTML = count;
+            count++;
+            rowDiv.appendChild(columnDiv);
+        }
+        nodes.push(rowDiv);
     }
-    nodes.push(rowDiv);
-  }
-  boxSelector.replaceChildren(...nodes);
+    boxSelector.replaceChildren(...nodes);
 };
 
 ratioSelecter.addEventListener("input", () => {
-  if (ratioSelecter.valueAsNumber <= goalSelector.valueAsNumber) {
-    goalSelector.value = ratioSelecter.valueAsNumber;
-    setInnerHTMLValue("#goal-number", goalSelector.valueAsNumber);
-  }
-  setInnerHTMLValue(
-    "#ratio-number",
-    `${ratioSelecter.valueAsNumber} x ${ratioSelecter.valueAsNumber}`
-  );
-  createBoxes(ratioSelecter.valueAsNumber);
+    if (ratioSelecter.valueAsNumber <= goalSelector.valueAsNumber) {
+        goalSelector.value = ratioSelecter.valueAsNumber;
+        setInnerHTMLValue("#goal-number", goalSelector.valueAsNumber);
+    }
+    setInnerHTMLValue(
+        "#ratio-number",
+        `${ratioSelecter.valueAsNumber} x ${ratioSelecter.valueAsNumber}`
+    );
+    createBoxes(ratioSelecter.valueAsNumber);
 });
 
 goalSelector.addEventListener("input", () => {
-  if (goalSelector.valueAsNumber >= ratioSelecter.valueAsNumber) {
-    ratioSelecter.value = goalSelector.valueAsNumber;
-    setInnerHTMLValue(
-      "#ratio-number",
-      `${ratioSelecter.valueAsNumber} x ${ratioSelecter.valueAsNumber}`
-    );
+    if (goalSelector.valueAsNumber >= ratioSelecter.valueAsNumber) {
+        ratioSelecter.value = goalSelector.valueAsNumber;
+        setInnerHTMLValue(
+            "#ratio-number",
+            `${ratioSelecter.valueAsNumber} x ${ratioSelecter.valueAsNumber}`
+        );
 
-    createBoxes(ratioSelecter.valueAsNumber);
-  }
-  setInnerHTMLValue("#goal-number", goalSelector.valueAsNumber);
+        createBoxes(ratioSelecter.valueAsNumber);
+    }
+    setInnerHTMLValue("#goal-number", goalSelector.valueAsNumber);
 });
 
 playerSelector.addEventListener("input", () => {
-  const setNewRatioValue = (playerSelector.valueAsNumber - 1) * 3;
-  if (ratioSelecter.valueAsNumber < setNewRatioValue) {
-    ratioSelecter.value = setNewRatioValue;
-    setInnerHTMLValue(
-      "#ratio-number",
-      `${ratioSelecter.valueAsNumber} x ${ratioSelecter.valueAsNumber}`
-    );
-    createBoxes(ratioSelecter.valueAsNumber);
-  }
-  setInnerHTMLValue("#player-number", playerSelector.valueAsNumber);
+    const setNewRatioValue = (playerSelector.valueAsNumber - 1) * 3;
+    if (ratioSelecter.valueAsNumber < setNewRatioValue) {
+        ratioSelecter.value = setNewRatioValue;
+        setInnerHTMLValue(
+            "#ratio-number",
+            `${ratioSelecter.valueAsNumber} x ${ratioSelecter.valueAsNumber}`
+        );
+        createBoxes(ratioSelecter.valueAsNumber);
+    }
+    setInnerHTMLValue("#player-number", playerSelector.valueAsNumber);
 });
