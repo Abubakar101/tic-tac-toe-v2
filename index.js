ac948f83bfc8cfd9e038907eadee56b9162908f6
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2023-03-11 19:45:51 -0500

    add win message temp

--- index.js
+++ index.js
@@ -4,70 +4,69 @@
 
 const directions = {
   0: {
-    x: -1,
     y: 0,
+    x: -1,
     o: 4,
   },
   1: {
-    x: -1,
     y: -1,
+    x: -1,
     o: 5,
   },
   2: {
-    x: 0,
     y: -1,
+    x: 0,
     o: 6,
   },
   3: {
-    x: +1,
     y: -1,
+    x: +1,
     o: 7,
   },
   4: {
-    x: +1,
     y: 0,
+    x: +1,
     o: 0,
   },
   5: {
-    x: +1,
     y: +1,
+    x: +1,
     o: 1,
   },
   6: {
-    x: 0,
     y: +1,
+    x: 0,
     o: 2,
   },
   7: {
-    x: -1,
     y: +1,
+    x: -1,
     o: 3,
   },
 };
 
-const validatePlayerChoices = (array, x, y, goal) => {
+// rowIndex (y) (up/down) and columnIndex (x) (left/right)
+const validatePlayerChoices = (array, y, x, goal) => {
   const currentValue = array[y][x];
   for (let key in directions) {
     const dir = directions[key];
     const oppDir = directions[dir.o];
+
     let totalCounts = 1;
+    let tmp_y = y + dir.y;
+    let tmp_x = x + dir.x;
+    while (array[tmp_y]?.[tmp_x] === currentValue) {
+      totalCounts++;
+      tmp_y = tmp_y + dir.y;
+      tmp_x = tmp_x + dir.x;
+    }
 
-    if (array[y]?.[x] === currentValue) {
-      let tmp_x = x + dir.x;
-      let tmp_y = y + dir.y;
-      while (array[tmp_y]?.[tmp_x] === currentValue) {
-        totalCounts++;
-        tmp_x = tmp_x + dir.x;
-        tmp_y = tmp_y + dir.y;
-      }
-
-      let tmp_oppDir_x = x + oppDir.x;
-      let tmp_oppDir_y = y + oppDir.y;
-      while (array[tmp_oppDir_y]?.[tmp_oppDir_x] === currentValue) {
-        totalCounts++;
-        tmp_oppDir_x = tmp_oppDir_x + oppDir.x;
-        tmp_oppDir_y = tmp_oppDir_y + oppDir.y;
-      }
+    let tmp_oppDir_y = y + oppDir.y;
+    let tmp_oppDir_x = x + oppDir.x;
+    while (array[tmp_oppDir_y]?.[tmp_oppDir_x] === currentValue) {
+      totalCounts++;
+      tmp_oppDir_y = tmp_oppDir_y + oppDir.y;
+      tmp_oppDir_x = tmp_oppDir_x + oppDir.x;
     }
 
     if (totalCounts >= goal) {
@@ -167,22 +166,18 @@ boxSelector.addEventListener("click", (event) => {
     if (row.className === "row") {
       const rowIndex = row.getAttribute("data-row");
       const columnIndex = column.getAttribute("data-column");
-      console.log(
-        ">>>>",
-        rowIndex,
-        columnIndex,
-        goalSelector.valueAsNumber,
-        initRowArray
-      );
       initRowArray[rowIndex][columnIndex] = 1;
       const validation = validatePlayerChoices(
         initRowArray,
-        rowIndex,
-        columnIndex,
+        Number(rowIndex),
+        Number(columnIndex),
         goalSelector.valueAsNumber
       );
 
       console.log(">>> validation", validation);
+      if (validation) {
+        setInnerHTMLValue("#message", "YOU WIN");
+      }
     }
   }
 });
